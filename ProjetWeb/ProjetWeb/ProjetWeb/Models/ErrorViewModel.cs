d4145f877fd29a6951850ea6aa1e6bc41e8ea149
SnowB0mb <patrysimon@videotron.ca>
2024-11-09 15:25:21 -0500

    réorganisation des fichiers
    RÉorganisation des fichiers pour qu'ils soient tous au meme endroit dans le projet
    Ceci causait des problèmes avec le .gitignore

--- ProjetWeb/ProjetWeb/ProjetWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
